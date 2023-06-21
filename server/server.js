2d3c990c8412871edda3c4a48afc1360e2656391
andyjwang19 <andyjwang19@gmail.com>
2023-06-21 16:51:24 -0400

    to https

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 // import express from 'express';
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const https = require('https');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const options = {
+    key: fs.readFileSync('keys/client-key.pem'),
+    cert: fs.readFileSync('keys/client-cert.pem'),
+};
+
+const app = express();
 
 const port = 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const routes = require('./routes/routes.tsx')(app, fs);
 
-const server = app.listen(port, () => {
-    console.log(`Express is listening at http://localhost:${port}`);
-});
+https.createServer(options, app).listen(port);
+// const server = app.listen(port, () => {
+//     console.log(`Express is listening at http://localhost:${port}`);
+// });
